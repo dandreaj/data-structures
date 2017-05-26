fc76f90daac22d460a3b29b7b6bea4e01cbf2bca
Jessica D'Andrea <jessica.dandrea@pwc.com>
2017-05-25 22:34:26 -0500

    completed stacks for all instantiation patterns all test passing

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage : {},
+    count : 0
+  };
+  _.extend(someInstance,stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.size = function() {
+  return this.count;
+};
+
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+stackMethods.pop = function() {
+  if (this.count > 0) {
+    this.count--;
+    var popped_item = this.storage[this.count];
+    delete this.storage[this.count];
+    return popped_item;
+  }
+};
+/*stackMethods.push = function(){
+
+}*/
 
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value; //storage.push(value) {0:value,1:value,2:value}
+    size++;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      size--;
+      var popped_item = storage[size];
+      delete storage[size];
+      return popped_item;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
