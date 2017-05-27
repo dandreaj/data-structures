405fb978021f0c674358b0c7bb658582703bd481
Jessica D'Andrea <jessica.dandrea@pwc.com>
2017-05-27 13:01:49 -0500

    Implemented queue in functional instatiation pattern all tests passing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,34 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var first = 0;
+  var last = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[last] = value;
+    last++;
+    size++;
   };
 
+  //{0:a} size =1 , first =0
+  //{1:b} size =1 first=1
+
+
   someInstance.dequeue = function() {
+    if(size > 0) {
+      var popped_item = storage[first];
+      delete storage[first];
+      first++;
+      size--;
+      return popped_item;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
