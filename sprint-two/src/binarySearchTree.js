9c995d5d3d0ff18b64d254a5acfcfb1eab8e2640
Jessica D'Andrea <jessica.dandrea@pwc.com>
2017-06-03 15:44:55 -0500

    Working through Binary Search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,35 @@
 var BinarySearchTree = function(value) {
+  var newBinaryTree = {};
+  newBinaryTree.value = value;
+  newBinaryTree.left = null;
+  newBinaryTree.right = null;
+
+  _.extend(newBinaryTree, binaryTreeMethods);
+  return newBinaryTree;
+};
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value) {
+  if(this === undefined) {
+    new BinarySearchTree(value);
+  } else {
+      if(value < this.value) {
+      this.left = value;
+    } else {
+      this.right = value;
+  }
+  }
+
+
+};
+
+binaryTreeMethods.contains = function() {
+
+};
+
+binaryTreeMethods.depthFirstLog = function() {
+
 };
 
 
